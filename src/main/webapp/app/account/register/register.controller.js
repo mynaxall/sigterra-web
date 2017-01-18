f3262ac5efc198c4bc52eb9bffffe9898db27af0
Alexander Sirotin <alexander@itomy.ch>
2017-01-18 12:52:47 +0200

    registration UI

--- src/main/webapp/app/account/register/register.controller.js
+++ src/main/webapp/app/account/register/register.controller.js
@@ -6,11 +6,13 @@
         .controller('RegisterController', RegisterController);
 
 
-    RegisterController.$inject = [ '$timeout', 'Auth', 'LoginService'];
+    RegisterController.$inject = [ '$timeout', 'Auth', 'LoginService', '$state'];
 
-    function RegisterController ($timeout, Auth, LoginService) {
+    function RegisterController ($timeout, Auth, LoginService, $state) {
         var vm = this;
 
+
+        vm.isFirstStep = true;
         vm.doNotMatch = null;
         vm.error = null;
         vm.errorUserExists = null;
@@ -18,7 +20,14 @@
         vm.register = register;
         vm.registerAccount = {};
         vm.success = null;
+        vm.isDone = false;
+        vm.skip = skip;
 
+        vm.nextStep = nextStep;
+
+        function nextStep (){
+            vm.isFirstStep = false;
+        }
         $timeout(function (){angular.element('#login').focus();});
 
         function register () {
@@ -31,13 +40,50 @@
                 vm.errorUserExists = null;
                 vm.errorEmailExists = null;
 
-                Auth.createAccount(vm.registerAccount).then(function () {
+                Auth.createAccount(vm.registerAccount).then(function (response) {
                     vm.success = 'OK';
+                    vm.isDone = true;
                 }).catch(function (response) {
                     vm.success = null;
                     if (response.status === 400 && response.data === 'login already in use') {
                         vm.errorUserExists = 'ERROR';
                     } else if (response.status === 400 && response.data === 'e-mail address already in use') {
+                        vm.isFirstStep = true;
+                        vm.errorEmailExists = 'ERROR';
+                    } else {
+                        vm.error = 'ERROR';
+                    }
+                });
+            }
+        }
+
+        function skip () {
+            if (vm.registerAccount.password !== vm.confirmPassword) {
+                vm.doNotMatch = 'ERROR';
+            } else {
+                vm.registerAccount.langKey =  'en' ;
+                vm.doNotMatch = null;
+                vm.error = null;
+                vm.errorUserExists = null;
+                vm.errorEmailExists = null;
+
+                var skipRegister ={"username": vm.registerAccount.username,
+                    "email": vm.registerAccount.email,
+                    "password": vm.registerAccount.password
+
+                };
+
+
+
+                Auth.createAccount(skipRegister).then(function (response) {
+                    vm.success = 'OK';
+                    vm.isDone = true;
+                }).catch(function (response) {
+                    vm.success = null;
+                    if (response.status === 400 && response.data === 'login already in use') {
+                        vm.errorUserExists = 'ERROR';
+                    } else if (response.status === 400 && response.data === 'e-mail address already in use') {
+                        vm.isFirstStep = true;
                         vm.errorEmailExists = 'ERROR';
                     } else {
                         vm.error = 'ERROR';
