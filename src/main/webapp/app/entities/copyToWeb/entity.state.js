4a33c512ecbad6e203005d496e3d42224d6d5298
naxall <kyryll.kravtsov@gmail.com>
2017-03-13 16:19:42 +0200

    Fixed building project with gradle and gulp with prod profile

--- src/main/webapp/app/entities/copyToWeb/entity.state.js
+++ src/main/webapp/app/entities/copyToWeb/entity.state.js
@@ -8,7 +8,7 @@
     stateConfig.$inject = ['$stateProvider'];
 
     function stateConfig($stateProvider) {
-        $stateProvider.state('entity', {
+        $stateProvider.state('entityCopy', {
             abstract: true,
             parent: 'app'
         });
