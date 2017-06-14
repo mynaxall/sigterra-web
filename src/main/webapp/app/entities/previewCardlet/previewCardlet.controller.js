9a5254f4a4c085281447b80c08f9b0ae03b1d5e9
Alexander Sirotin <alexander@itomy.ch>
2017-06-14 19:25:50 +0300

    SGTRR-661 Preview page: draft functionality

--- src/main/webapp/app/entities/previewCardlet/previewCardlet.controller.js
+++ src/main/webapp/app/entities/previewCardlet/previewCardlet.controller.js
@@ -7,21 +7,94 @@
 
 
 
-    PreviewCardletController.$inject = ['$scope', '$state', 'UserCardletList', 'ParseLinks', 'AlertService', 'pagingParams', 'paginationConstants', '$http', '$timeout', '$location', 'LoginService'];
+    PreviewCardletController.$inject = ['$scope', '$state', 'UserCardletList', 'ParseLinks', 'AlertService', 'pagingParams', 'paginationConstants', '$http', '$timeout', '$location', 'LoginService', '$sce'];
 
-    function PreviewCardletController ($scope, $state, CardletList, ParseLinks, AlertService, pagingParams, paginationcardletConstants ,$http, $timeout, $location, LoginService) {
+    function PreviewCardletController ($scope, $state, CardletList, ParseLinks, AlertService, pagingParams, paginationcardletConstants ,$http, $timeout, $location, LoginService, $sce) {
 
+        var vm = this;
 
+        vm.currentSlide = 0;
 
         $scope.getCardlet = function(){
             var param1 = $location.search().cardletId;
             $http.get("/api/cardlet/"+param1)
                 .success(function(response, status, headers) {
                     $scope.tabNames = response;
+                    if($scope.tabNames.tabs[0].tabType == 1){
+                        $scope.currentUrl = $scope.createURL($scope.tabNames.tabs[0].site.value);
+                        $scope.tabType = 1
+                        $scope.currentLink =  $scope.getLink($scope.tabNames.tabs[0].site.value);
+                    }
+                    else{
+                        var buttons = document.getElementsByClassName("carousel-control");
+
+                        for(var j = 0; j < buttons.length; j++){
+                            document.getElementsByClassName("carousel-control")[j].className += " disabledLink"
+                        }
+                        $scope.currentUrl = $scope.createURL($scope.tabNames.tabs[0].items[0].link);
+                        $scope.currentLink = $scope.getLink($scope.tabNames.tabs[0].items[0].link);
+                        $scope.tabType = $scope.tabNames.tabs[0].tabType
+                    }
                 });
         };
 
+        $scope.trustSrc = function(src) {
+            return $sce.trustAsResourceUrl(src);
+        };
 
+        $scope.createURL = function(link){
+            if(link.indexOf('https://') == -1 && link.indexOf('http://') == -1){
+               link = "http://"+link
+            }else if(link.indexOf('https://') != -1){
+                link.replace("https://", "http://")
+
+            }
+            return link
+        }
+
+
+        $scope.prevSlide = function(index){
+            $scope.currentUrl = undefined
+            if( vm.currentSlide == 0){
+                vm.currentSlide = $scope.tabNames.tabs[index].items.length -1
+            }else{
+                vm.currentSlide = vm.currentSlide - 1;
+            }
+            console.log(vm.currentSlide)
+            setTimeout(function() {
+                $scope.currentUrl = $scope.createURL($scope.tabNames.tabs[index].items[vm.currentSlide].link);
+                $scope.currentLink = $scope.getLink($scope.tabNames.tabs[index].items[vm.currentSlide].link);
+            },10);
+        };
+
+        $scope.nextSlide = function(index){
+            $scope.currentUrl = undefined
+            if( vm.currentSlide == $scope.tabNames.tabs[index].items.length -1){
+                vm.currentSlide = 0
+            }else{
+                vm.currentSlide = vm.currentSlide + 1;
+            }
+            setTimeout(function() {
+                $scope.currentUrl = $scope.createURL($scope.tabNames.tabs[index].items[vm.currentSlide].link);
+                $scope.currentLink = $scope.getLink($scope.tabNames.tabs[index].items[vm.currentSlide].link);
+            },10)
+        };
+
+        $scope.getLink = function(link){
+            var linkNew = link;
+            if(link.indexOf('https://') != -1){
+                linkNew = link.replace("https://", "");
+            }else if(link.indexOf('http://') != -1){
+                linkNew = link.replace("http://", "")
+            }
+            return linkNew;
+
+        }
+
+
+        $scope.next = function(){
+            console.log("asd")
+        }
 
         function transition () {
             $state.transitionTo($state.$current, {
@@ -31,9 +104,27 @@
             });
         }
 
+        $scope.openCity = function(cardName, cardId, id) {
+            $scope.currentUrl = undefined;
+            var buttons = document.getElementsByClassName("carousel-control");
 
-        $scope.openCity = function(cardName, cardId) {
+            for(var j = 0; j < buttons.length; j++){
+                document.getElementsByClassName("carousel-control")[j].className += " disabledLink"
+            }
 
+            vm.currentSlide = 0;
+            setTimeout(function() {
+                if ($scope.tabNames.tabs[id].tabType == 1) {
+                    $scope.currentUrl = $scope.createURL($scope.tabNames.tabs[id].site.value);
+                    $scope.currentLink = $scope.getLink($scope.tabNames.tabs[id].site.value);
+                    $scope.tabType = $scope.tabNames.tabs[id].tabType
+                }
+                else {
+                    $scope.currentUrl = $scope.createURL($scope.tabNames.tabs[id].items[0].link);
+                    $scope.currentLink = $scope.getLink($scope.tabNames.tabs[id].items[0].link);
+                    $scope.tabType = $scope.tabNames.tabs[id].tabType
+                }
+            },10)
 
             var i, tabcontent2, tablinks2, tabs2;
             tabcontent2 = document.getElementsByClassName("tabcontent2");
@@ -54,7 +145,7 @@
 
             document.getElementById(cardName).style.display = "block";
             document.getElementById(cardId).className += " active";
-        }
+        };
 
 
         $scope.showSignature = function(){
@@ -62,7 +153,7 @@
                 .success(function(response, status, headers) {
                     $scope.signatures = response;
                 });
-        }
+        };
 
 
 
@@ -77,38 +168,36 @@
 
         $scope.addColors = function(id, colorMain, colorSecond){
 
-            var cyrrentEl = document.getElementById(id);
+            var currentEl = document.getElementById(id);
 
-            if(cyrrentEl) {
-                cyrrentEl.style.background = "#F9F9F9";
-                cyrrentEl.style.borderTop = "1px solid #D0D8D9"
-                cyrrentEl.style.borderBottom = "1px solid #D0D8D9";
+            if(currentEl) {
+                currentEl.style.background = "#F9F9F9";
+                currentEl.style.borderTop = "1px solid #D0D8D9"
+                currentEl.style.borderBottom = "1px solid #D0D8D9";
                 if (angular.element(document.getElementById(id)).hasClass('active')) {
-                    cyrrentEl.style.background = "#FFFFFF";
-                    cyrrentEl.style.borderTop = "2px solid #" + colorSecond
-                    cyrrentEl.style.borderBottom = "0px";
+                    currentEl.style.background = "#FFFFFF";
+                    currentEl.style.borderTop = "2px solid #" + colorSecond;
+                    currentEl.style.borderBottom = "0px";
                 }
 
                 if ($scope.tabNames) {
 
                     if ($scope.tabNames.tabs.length == 1) {
-                        cyrrentEl.style.width = "540px"
+                        currentEl.style.width = "540px"
                     }
-
                     if ($scope.tabNames.tabs.length === 2) {
-                        cyrrentEl.style.width = "270px"
+                        currentEl.style.width = "270px"
                     }
                     if ($scope.tabNames.tabs.length === 3) {
-                        cyrrentEl.style.width = "180px"
+                        currentEl.style.width = "180px"
                     }
                     if ($scope.tabNames.tabs.length === 4) {
-                        cyrrentEl.style.width = "135px"
+                        currentEl.style.width = "135px"
                     }
                 }
             }
-        }
+        };
 
-        $scope.myInterval = 3000;
 
     }
 
