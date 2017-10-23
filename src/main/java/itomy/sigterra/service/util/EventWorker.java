99330858c6e817eff1cae7250b50057c11934e6a
Mykhailo Zenkov <mykhailo.zenkov@itomy.ch>
2017-10-23 18:40:38 +0300

    SGTRR-1515 Portfolio the user can't delete signature if it has activity in activity page: fixed event processing on cardlet delete

--- src/main/java/itomy/sigterra/service/util/EventWorker.java
+++ src/main/java/itomy/sigterra/service/util/EventWorker.java
@@ -4,7 +4,9 @@ import itomy.sigterra.config.JHipsterProperties;
 import itomy.sigterra.domain.Event;
 import itomy.sigterra.domain.User;
 import itomy.sigterra.domain.Visitor;
+import itomy.sigterra.repository.CardletRepository;
 import itomy.sigterra.repository.EventRepository;
+import itomy.sigterra.repository.ItemRepository;
 import itomy.sigterra.service.VisitorService;
 import itomy.sigterra.service.dto.EventProcessDTO;
 import org.slf4j.Logger;
@@ -18,6 +20,8 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import static itomy.sigterra.domain.enumeration.EventType.READ;
+
 @Service
 public class EventWorker {
     private static final Logger log = LoggerFactory.getLogger(EventWorker.class);
@@ -26,13 +30,22 @@ public class EventWorker {
 
     private VisitorService visitorService;
     private EventRepository eventRepository;
+    private CardletRepository cardletRepository;
+    private ItemRepository itemRepository;
+
     private final Integer processEventDelay;
     private final Integer processEventInitialDelay;
     private final TimeUnit processEventDelayTimeUnit;
 
-    public EventWorker(VisitorService visitorService, EventRepository eventRepository, JHipsterProperties jHipsterProperties) {
+    public EventWorker(VisitorService visitorService,
+                       EventRepository eventRepository,
+                       JHipsterProperties jHipsterProperties,
+                       CardletRepository cardletRepository,
+                       ItemRepository itemRepository) {
         this.visitorService = visitorService;
         this.eventRepository = eventRepository;
+        this.cardletRepository = cardletRepository;
+        this.itemRepository = itemRepository;
 
         JHipsterProperties.EventWorker eventWorker = jHipsterProperties.getEventWorker();
         this.processEventDelay = eventWorker.getProcessEventDelay();
@@ -61,10 +74,23 @@ public class EventWorker {
                 if (event != null) {
                     Visitor visitor = getVisitor(eventProcessDTO);
                     event.setVisitor(visitor);
-                    eventRepository.save(event);
+                    trySaveEvent(event);
                 }
             } catch (Exception ex) {
                 log.error("Can't process event", ex);
+            }
+        }
+    }
+
+    private void trySaveEvent(Event event) {
+        if (event.getType().equals(READ)) {
+            if (cardletRepository.exists(event.getCardlet().getId())
+                && itemRepository.exists(event.getItem().getId())) {
+                eventRepository.save(event);
+            }
+        } else {
+            if (cardletRepository.exists(event.getCardlet().getId())) {
+                eventRepository.save(event);
             }
         }
     }
