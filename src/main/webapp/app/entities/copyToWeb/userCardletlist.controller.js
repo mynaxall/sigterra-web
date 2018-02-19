88d4b0c3a3d7fd94a724d52358c66a9d16dbe358
Alexander Sirotin <alexander@itomy.ch>
2018-02-19 15:05:27 +0200

    SGTRR-2435 Make Hexa code longer: done

--- src/main/webapp/app/entities/copyToWeb/userCardletlist.controller.js
+++ src/main/webapp/app/entities/copyToWeb/userCardletlist.controller.js
@@ -16,9 +16,13 @@
         vm.showCarousel = true;
         $scope.showLink = true;
 
+        function  getId(id) {
+            return  window.atob( window.atob( window.atob(window.atob(id))));
+        }
+
         $scope.getCardlet = function(){
             var param1 = $location.search().cardletId;
-            $http.get("/api/cardlet/"+window.atob(param1))
+            $http.get("/api/cardlet/"+ getId(param1))
                 .success(function(response, status, headers) {
                     $scope.tabNames = response;
                 });
