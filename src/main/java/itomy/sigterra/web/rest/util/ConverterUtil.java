03fb94820dd288ad5c0ad660618af4ca4cf5a1ce
artem.diachenko <artem.diachenko@itomy.ch>
2018-02-22 17:18:34 +0200

    SGTRR-2456: delete useless code

--- src/main/java/itomy/sigterra/web/rest/util/ConverterUtil.java
+++ src/main/java/itomy/sigterra/web/rest/util/ConverterUtil.java
@@ -4,6 +4,7 @@ package itomy.sigterra.web.rest.util;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 import org.apache.commons.io.IOUtils;
+
 import java.io.*;
 
 public class ConverterUtil {
@@ -20,17 +21,10 @@ public class ConverterUtil {
         return convertFile;
     }
 
-    public static MultipartFile convertJavaFileToMultipartFile(File file) {
+    public static MultipartFile convertJavaFileToMultipartFile(File file) throws IOException {
         MultipartFile multipartFile = null;
-        try {
-            FileInputStream input = new FileInputStream(file);
-            multipartFile = new MockMultipartFile("file", file.getName(), "text/plain", IOUtils.toByteArray(input));
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        FileInputStream input = new FileInputStream(file);
+        multipartFile = new MockMultipartFile("file", file.getName(), "text/plain", IOUtils.toByteArray(input));
         return multipartFile;
     }
 }
